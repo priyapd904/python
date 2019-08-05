a035dbfc9a74951c069607a383fee55b78e926a6
priyapd904 <53601382+priyapd904@users.noreply.github.com>
2019-08-05 14:28:08 +0545

    Update thrice.py

--- thrice.py
+++ thrice.py
@@ -1,1 +1,8 @@
-
+a=int(input("enter the number")
+ b=int(input("enter the number")
+       c=int(input("enter the number")
+             sum=a+b+c
+             if a=b=c:
+             print(sum*3)
+             else:
+             print(sum)
