6f030173cb091e9ab37198a7eb6601a0101b825e
priyapd904 <53601382+priyapd904@users.noreply.github.com>
2019-08-06 12:15:46 +0530

    Create add item in tuple.py

---
+++ add item in tuple.py
@@ -0,0 +1,11 @@
+tuplex=(4,6,2,8,3,1)
+print(tuplex)
+print=tuplex+(9)
+print(tuplex)
+tuplex=tuplex[:5]+(15,20,25)+tuplex[5:]
+print(tuplex)
+listx=list tuplex
+listx.append(30)
+listx.extend([20,21,22])
+tuplex=tuple(listx)
+print(tuplex)
